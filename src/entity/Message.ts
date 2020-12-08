67390c0bfeddf550d36d72332aed8a3a09d80db3
Bruce Bryant <793041663@qq.com>
2020-12-09 01:10:30 +0800

    Message Entity

---
+++ src/entity/Message.ts
@@ -0,0 +1,45 @@
+import { Field, ID, ObjectType } from "type-graphql";
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from "typeorm";
+import { User } from "./User";
+
+@ObjectType()
+@Entity()
+export class Message extends BaseEntity {
+  @Field(() => ID)
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Field()
+  @Column("timestamp")
+  createdAt: Date;
+
+  @Field()
+  @ManyToOne(() => User, (user) => user.email)
+  user: User;
+
+  @Field()
+  @Column("text")
+  content: string;
+
+  @Field()
+  @Column("text")
+  image?: string;
+
+  @Field()
+  @Column("int")
+  numberOfComments?: number;
+
+  @Field()
+  @Column("int")
+  numberOfLikes?: number;
+
+  @Field()
+  @Column("int")
+  numberOfRetweets?: number;
+}
